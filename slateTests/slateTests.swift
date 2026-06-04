83695b23e1a24230f86a9cbdac17eecf7ae77950
Aziz Bibitov <azizbibitov11@gmail.com>
2026-06-04 19:26:42 +0500

    Initial Commit

---
+++ slateTests/slateTests.swift
@@ -0,0 +1,16 @@
+//
+//  slateTests.swift
+//  slateTests
+//
+//  Created by Aziz Bibitov on 04.06.2026.
+//
+
+import Testing
+
+struct slateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
